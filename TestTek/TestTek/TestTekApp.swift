792b030dc381fd5ff2d7841887b012adca69c661
Nicolas Bellon <nicolas@foodvisor.io>
2022-08-02 10:45:43 +0200

    Initial commit

---
+++ TestTek/TestTek/TestTekApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTekApp.swift
+//  TestTek
+//
+//  Created by Nicolas Bellon on 02/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
